64cabb4a598ec0934de675bbb719071b4ace5ede
bananu7 <bananu7@o2.pl>
2021-09-28 14:13:07 +0200

    Actually put the impl of async mergesort

--- mergesort_async_inside.cpp
+++ mergesort_async_inside.cpp
@@ -1,30 +1,34 @@
 #include <vector>
 #include <iostream>
+#include <future>
 
 std::vector<int> mergesort(std::vector<int> v) {
 	if (v.size() <= 1)
 		return v;
 
-	std::vector<int> left(v.begin(), v.begin() + v.size()/2);
+	std::vector<int> left(v.begin(), v.begin() + v.size() / 2);
 	std::vector<int> right(v.begin() + v.size() / 2, v.end());
 
-	auto leftSorted = mergesort(std::move(left));
-	auto rightSorted = mergesort(std::move(right));
+	auto leftSortedF = std::async(std::launch::async, mergesort, std::move(left));
+	auto rightSortedF = std::async(std::launch::async, mergesort, std::move(right));
+
+	auto leftSorted = leftSortedF.get();
+	auto rightSorted = leftSortedF.get();
 
 	std::vector<int> result;
 	auto itL = leftSorted.begin();
 	auto itR = rightSorted.begin();
 
-	while (not (itL == leftSorted.end() and itR == rightSorted.end())) {
+	while (itL != leftSorted.end() or itR != rightSorted.end()) {
 		if (
 			(itL != leftSorted.end())
-			&&
+			and
 			(
 				(itR == rightSorted.end())
-				||
+				or
 				(*itL < *itR)
-			)
-		) {
+				)
+			) {
 			result.push_back(*itL);
 			itL++;
 		}
